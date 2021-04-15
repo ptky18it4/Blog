72b06119e08d6b85736b6b64fbd4ce00b572cd11
Trung Ky <phamtrungky14022000@gmail.com>
2021-04-15 19:55:17 +0700

    interface editing

--- app/src/main/java/com/academy/blog/MainActivity.kt
+++ app/src/main/java/com/academy/blog/MainActivity.kt
@@ -1,11 +1,4 @@
 package com.academy.blog
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+class MainActivity {
 }
